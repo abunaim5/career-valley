e59a8bac0eca79e6866069014095c3645d71a0c0
abunaim5 <abunaim140@gmail.com>
2024-03-23 22:36:04 +0600

    added job category component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import JobCategory from "../JobCategory/JobCategory";
 
 const Home = () => {
     return (
@@ -6,6 +7,7 @@ const Home = () => {
             <div>
                 <Banner></Banner>
             </div>
+            <JobCategory></JobCategory>
         </div>
     );
 };
---
+++ src/JobCategory/JobCategory.jsx
@@ -0,0 +1,21 @@
+const JobCategory = () => {
+    return (
+        <div className="max-w-[82.5rem] mx-auto">
+            <div className="text-center mt-32 space-y-4">
+                <h2 className="text-5xl font-extrabold">Job Category List</h2>
+                <p className="font-medium text-[#757575]">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div className="bg-gray-100 mt-8">
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <h3 className="text-xl font-extrabold">Account & Finance</h3>
+                    <p className="font-medium text-[#A3A3A3]">300 Jobs Available</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default JobCategory;
